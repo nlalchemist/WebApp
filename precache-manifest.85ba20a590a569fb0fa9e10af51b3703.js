62ac6d180eb9cfc605d94beaea06533fc9a09506
melancholy14 <melancholy8914@gmail.com>
2019-09-26 21:28:39 +0100

    Updates

---
+++ precache-manifest.85ba20a590a569fb0fa9e10af51b3703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b973b50f56a6f235884f87b49da941",
+    "url": "/WebApp/index.html"
+  },
+  {
+    "revision": "2b513813852edb6a0497",
+    "url": "/WebApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6ae944ff65d817f23e8",
+    "url": "/WebApp/static/js/2.30f39e88.chunk.js"
+  },
+  {
+    "revision": "2b513813852edb6a0497",
+    "url": "/WebApp/static/js/main.0caad108.chunk.js"
+  },
+  {
+    "revision": "a106c23f1f9bd0964022",
+    "url": "/WebApp/static/js/runtime-main.abbf4364.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/WebApp/static/media/logo.25bf045c.svg"
+  }
+]);
