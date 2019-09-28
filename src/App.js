6d2258de4389ffe74aa2d78352ba284fefe22062
melancholy14 <melancholy8914@gmail.com>
2019-09-28 19:22:54 +0100

    Update: add styled-components package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { googleMapAPI } from './utils/keys';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+
+  #root {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    & > div {
+      margin: 1rem;
+      position: relative;
+      height: calc(100% - 2rem);
+      width: calc(100% - 2rem);
+    }
+  }
+`;
 
 const mapStyles = {
   width: '100%',
@@ -12,12 +51,14 @@ export class MapContainer extends React.Component {
     const { google } = this.props;
 
     return (
-      <Map
-        google={google}
-        zoom={14}
-        style={mapStyles}
-        className="map"
-      />
+      <>
+        <GlobalStyle />
+        <Map
+          google={google}
+          zoom={14}
+          style={mapStyles}
+        />
+      </>
     );
   }
 }
